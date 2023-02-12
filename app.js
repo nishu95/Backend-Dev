082814a166bb9bdc213d636193930b08fbaf7dd3
nitish <nitishrock26@gmail.com>
2023-02-12 18:31:08 +0530

    vid 9 to 13

--- app.js
+++ app.js
@@ -10,42 +10,91 @@ const http = require("http");      // create a global http object to create a no
 
 // server.listen(3000);
 
-// response-request
+// // response-request
+// const server = http.createServer((req,res)=>{
+//     console.log("NITISH");
+//     console.log(req.url,req.method,req.headers);
+//     res.setHeader("Content-Type","text/html");
+//     const url= req.url;
+//     if(url === "/"){
+//         res.write("<html>");
+//         res.write("<head><title>My First Page</title></head>");
+//         res.write("<body><h1>Hello My Node.js Server</h1></body>");
+//         res.write("</html>");
+//     }
+//     if(url === "/home"){
+//         res.write("<html>");
+//         res.write("<head><title>My First Page</title></head>");
+//         res.write("<body><h1>Welcome Home</h1></body>");
+//         res.write("</html>");
+//     }
+//     if(url === "/about"){
+//         res.write("<html>");
+//         res.write("<head><title>My First Page</title></head>");
+//         res.write("<body><h1>Welcome About</h1></body>");
+//         res.write("</html>");
+//     }
+//     if(url === "/node"){
+//         res.write("<html>");
+//         res.write("<head><title>My First Page</title></head>");
+//         res.write("<body><h1>Welcome node.js project</h1></body>");
+//         res.write("</html>");
+//     }
+
+
+//     res.end();
+
+// });
+
+// server.listen(3000);
+
+
+// video 9 to 13
+const fs=require("fs");
+
 const server = http.createServer((req,res)=>{
-    console.log("NITISH");
-    console.log(req.url,req.method,req.headers);
     res.setHeader("Content-Type","text/html");
     const url= req.url;
+    const method=req.method;
     if(url === "/"){
+        fs.readFile("message.txt",{encoding:"utf-8"},(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log("data from file:"+data);
+            res.write("<html>");
+            res.write("<head><title>My First Page</title></head>");
+            res.write(`<body>${data}</body>`);
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+            res.write("</html>");
+            return res.end();
+        });
+    }
+    else if(url=== "/message" && method === 'POST'){
+        const body=[];
+        req.on("data",(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        return req.on("end",()=>{
+            const parsedBody=Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const InputMessage=parsedBody.split("=")[1];
+            fs.writeFile("message.txt",InputMessage,(err)=>{
+                res.statusCode=302;
+                res.setHeader("location","/");
+                return res.end();
+            });
+        });
+    }
+    else{
         res.write("<html>");
         res.write("<head><title>My First Page</title></head>");
         res.write("<body><h1>Hello My Node.js Server</h1></body>");
         res.write("</html>");
+        return res.end();
     }
-    if(url === "/home"){
-        res.write("<html>");
-        res.write("<head><title>My First Page</title></head>");
-        res.write("<body><h1>Welcome Home</h1></body>");
-        res.write("</html>");
-    }
-    if(url === "/about"){
-        res.write("<html>");
-        res.write("<head><title>My First Page</title></head>");
-        res.write("<body><h1>Welcome About</h1></body>");
-        res.write("</html>");
-    }
-    if(url === "/node"){
-        res.write("<html>");
-        res.write("<head><title>My First Page</title></head>");
-        res.write("<body><h1>Welcome node.js project</h1></body>");
-        res.write("</html>");
-    }
-
-    
-    res.end();
-
 });
 
 server.listen(3000);
-
-
