97e4dfac1935946988d6241e65bf95e9db40a46f
nitish <nitishrock26@gmail.com>
2023-07-14 02:13:19 +0530

    dynamic routes part 1

--- product.js
+++ product.js
@@ -1,80 +1,35 @@
-
-/*
-// const products=[];
-const fs = require('fs');
-const path = require('path');
-
-module.exports = class Product {
-    constructor(t){
-        this.title=t;
-    }
-
-    save() {
-        // products.push(this);
-        const p = path.join(path.dirname(require.main.filename),'data','products.json');
-        fs.readFile(p,(err,fileContent)=>{
-            let products=[];
-            if(!err){
-                products = JSON.parse(JSON.stringify(fileContent));
-            }
-            products.push(this); // error
-            fs.writeFile(p,JSON.stringify(products) , err=>{
-                console.log(err);
-            });
-        });
-    }
-
-    static fetchAll(cb){   // cb is callback function
-        const p = path.join(path.dirname(require.main.filename),'data','products.json');
-        fs.readFile(p,(err,fileContent)=>{
-            console.log("this is the  file content");
-            console.log(fileContent);
-            if(err){
-                // return [];
-                console.log("json file is empty");
-                cb([]);
-
-            }
-            // return JSON.parse(fileContent);
-            else{
-            cb(JSON.parse(JSON.stringify(fileContent)));
-            }
-        });
-    }
-}
-*/
-
 const fs = require('fs');
 const path = require('path');
 
 const p = path.join(
-    path.dirname(require.main.filename),
-    'data',
-    'products.json'
+  path.dirname(require.main.filename),
+  'data',
+  'products.json'
 );
 
-const getProductsFromFile = cb =>{
-    
-      fs.readFile(p, (err, fileContent) => {
-        if (err) {
-            cb([]);
-        }
-        else{
-            cb(JSON.parse(fileContent));
-        }
-        
-      });
-}
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile(products =>{
-        products.push(this);
-        fs.writeFile(p, JSON.stringify(products), err => {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
@@ -83,5 +38,12 @@ module.exports = class Product {
   static fetchAll(cb) {
     getProductsFromFile(cb);
   }
-};
 
+
+  static findById(id,cb){
+    getProductsFromFile(products => {
+      const product = products.find(p => p.id === id);
+      cb(product);
+    });
+  }
+};
